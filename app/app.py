b3e840c53487e78721332df27563d56c841ebf33
Dhrutabrata Biswal <dhrutabratabiswal@gmail.com>
2025-12-07 11:50:37 +0530

    api work perfec

---
+++ app/app.py
@@ -0,0 +1,21 @@
+from fastapi import FastAPI,HTTPException
+
+app = FastAPI()
+
+texts = {1:{"dhrutabrata":{"Biotechnology"}},
+         2:{"saloni":{"computer science"}}        
+        }
+
+@app.get("/home")
+def home():
+    return{"message":"hello"}
+
+@app.get("/hello")
+def hello():
+    return{"message":"do u know me"}
+
+@app.get("/post/{id}")
+def get_by_id(id:int):
+    if id not in texts:
+        raise HTTPException(status_code=404,detail="post not found")
+    return texts.get(id)
--- main.py
+++ main.py
@@ -1,6 +1,13 @@
-def main():
-    print("Hello from fastapi-fullstack-project!")
+# from fastapi import FastAPI
 
+# app = FastAPI()
+
+# @app.get("/home")
+# def home():
+#     return {"message":"helloworld"}
+
+import uvicorn
+from app.app import app
 
 if __name__ == "__main__":
-    main()
+    uvicorn.run(app,host="0.0.0.0",port=8000,reload=True)
